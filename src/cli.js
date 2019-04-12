ad2f1284880f4f00fdfa47e7361d8a4e6796589e
Jesse <jessetyoungblood@gmail.com>
2019-04-11 20:37:36 -0400

    Bugfix cli global install
    The __dirname for the .env library was not set correctly, so some settings would be ignored.

--- src/cli.js
+++ src/cli.js
@@ -2,4 +2,11 @@
 
 'use strict';
 
-require('./start.js');
+const path = require('path');
+
+const Server = require('./Server');
+const settings = require('env-smart').load({ lowercase: true, directory: path.join(__dirname, '../') });
+
+const server = new Server(settings);
+
+server.start();
